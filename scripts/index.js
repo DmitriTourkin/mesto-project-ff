d36588d5aba17bface4c7098230df7dc3938e165
Dmitrii Tourkin <ddiimmuuzz@yandex.ru>
2024-07-30 15:05:25 +0300

    Code fixed

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,13 @@
 const cardTemplate = document.querySelector('#card-template').content;
 const placesContainer = document.querySelector('.places__list');
 
-const createCard = (cardObj, deleteFunction) => {
+const createCard = (cardObj, deleteElement) => {
   const card = cardTemplate.querySelector('.places__item').cloneNode(true);
   const cardTitle = card.querySelector('.card__title');
   const cardImage = card.querySelector('.card__image');
 
   const deleteCardButton = card.querySelector('.card__delete-button');
-  deleteCardButton.addEventListener('click', () => deleteFunction(card));
+  deleteCardButton.addEventListener('click', () => deleteElement(card));
 
   cardTitle.textContent = cardObj.name;
   cardImage.src = cardObj.link;
@@ -16,13 +16,13 @@ const createCard = (cardObj, deleteFunction) => {
   return card;
 }
 
-const deleteCard = cardElement => {
-  cardElement.remove();
+const deleteElement = element => {
+  element.remove();
 }
 
 const renderCards = cardsArray => {
   cardsArray.forEach(cardItem => {
-    const cardElement = createCard(cardItem, deleteCard);
+    const cardElement = createCard(cardItem, deleteElement);
     placesContainer.append(cardElement);
   }
 )};
