caca677d9e3fe09b181d948fccc69caca77ff865
Dmitrii Tourkin <ddiimmuuzz@yandex.ru>
2024-09-14 14:32:56 +0300

    Настроен Webpack, Babel, PostCSS, gitignore

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import './pages/index.css';
+import { initialCards } from './scripts/cards';
+
 const cardTemplate = document.querySelector('#card-template').content;
 const placesContainer = document.querySelector('.places__list');
 
